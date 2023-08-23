db3be219c57caa8b72d4421135f41b13e829d26f
Paul Treanor <treanorpaul9@gmail.com>
2023-08-23 10:55:25 +0000

    Write solution, test, and notes for fibonacci

--- basic/fibonacci/fibonacci.ts
+++ basic/fibonacci/fibonacci.ts
@@ -1,5 +1,11 @@
 // Return the nth value in the fibonacci sequence
 export const fibonacci = (n: number): number => {
-	
-  return n
+  let prev: number = 0
+  let curr: number = 1
+  for (let i = 1; i < n; i++) {
+    const tmp: number = prev
+    prev = curr
+    curr = prev + tmp
+  }
+  return curr
 }
